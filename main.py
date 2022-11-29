559ab50680b54101e815df05b2558a6fb4fd86c3
Turner009 <009turner@gmail.com>
2022-11-29 04:29:43 -0600

    list and dictionary defined

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 # day trip genorator project
 
+cities = ["New York", "Chicago", "San Francisco", "Vicksburg, MS"]
+restaurants = ["Dallas BBQ", "Time Out Market", "Waffle House", "International Food Court"]
+transportation = ["Uber", "Cycling", "Walking", "Rent a Car"]
+entertanment = ["Museum Circuit", "Live Show Circuit", "Interactive Popup Circuit", "1 Museum, 1 Live Show, 1 Interactive Popup"]
+trip_selections = {"city":"", "food":"", "transportation":"", "entertainment":""}
+
 
 
