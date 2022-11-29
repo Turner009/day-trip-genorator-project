2d6cdb59b4dc2149dc383f2a5308ea9a38d7423d
Turner009 <009turner@gmail.com>
2022-11-29 07:10:19 -0600

    City Selection function complete

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
 # day trip genorator project
 
+import random
+
 cities = ["New York", "Chicago", "San Francisco", "Vicksburg, MS"]
 restaurants = ["Dallas BBQ", "Time Out Market", "Waffle House", "International Food Court"]
 transportation = ["Uber", "Cycling", "Walking", "Rent a Car"]
 entertanment = ["Museum Circuit", "Live Show Circuit", "Interactive Popup Circuit", "1 Museum, 1 Live Show, 1 Interactive Popup"]
 trip_selections = {"city":"", "food":"", "transportation":"", "entertainment":""}
+
+print("Welcome to the day trip genorator! Let get started.")
+
+def city():
+    trip_selections["city"] = random.choice(cities)
+    print("You will be traveling to", trip_selections["city"],".")
+    city_answer = input("Do you like this choice? (Y or N)")
+    if city_answer == "Y":
+        print("Confirmed")
+    while city_answer == "N":
+        trip_selections["city"] = random.choice(cities)
+        print(f"New selection is", trip_selections["city"],".")
+        city_answer = input("Do you like this choice? (Y or N)")
+    print(f"Your confirmed city choice is",trip_selections["city"], ".")
+             
+city()
+
+
+    
 
 
 
