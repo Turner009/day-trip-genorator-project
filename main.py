1ac57074733dcce8ce5657933a92b32d3b487819
Turner009 <009turner@gmail.com>
2022-11-29 08:16:05 -0600

    transportation function added

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ trip_selections = {"city":"", "food":"", "transportation":"", "entertainment":""
 
 print("Welcome to the day trip genorator! Let get started.")
 
+""""
 def city():
     trip_selections["city"] = random.choice(cities)
     print("You will be traveling to", trip_selections["city"],".")
@@ -18,11 +19,42 @@ def city():
         print("Confirmed")
     while city_answer == "N":
         trip_selections["city"] = random.choice(cities)
-        print(f"New selection is", trip_selections["city"],".")
+        print(f"Your new city selection is", trip_selections["city"],".")
         city_answer = input("Do you like this choice? (Y or N)")
     print(f"Your confirmed city choice is",trip_selections["city"], ".")
-             
-city()
+"""
+
+def food():
+    trip_selections["food"] = random.choice(restaurants)
+    print("Your lunch will be from", trip_selections["food"],".")
+    food_answer = input("Do you like this choice? (Y or N)")
+    if food_answer == "Y":
+        print("Confirmed")
+    while food_answer == "N":
+        trip_selections["food"] = random.choice(restaurants)
+        print(f"Your new food selection is", trip_selections["food"],".")
+        food_answer = input("Do you like this choice? (Y or N)")
+    print(f"Your confirmed food choice is", trip_selections["food"], ".")
+
+
+
+def transport():
+    trip_selections["transportation"] = random.choice(transportation)
+    print("Your transportation will be", trip_selections["transportation"],".")
+    ride_answer = input("Do you like this choice? (Y or N)")
+    if ride_answer == "Y":
+        print("Confirmed")
+    while ride_answer == "N":
+        trip_selections["transportation"] = random.choice(transportation)
+        print(f"Your new transportation selection is", trip_selections["transportation"],".")
+        ride_answer = input("Do you like this choice? (Y or N)")
+    print(f"Your confirmed transportation option is", trip_selections["transportation"], ".")
+
+# city()
+food()
+transport()
+
+ 
 
 
     
